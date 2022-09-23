6722131547e20d242965966ee8fff9bf4afb902d
devilhyt <devilhyt@gmail.com>
2022-09-23 11:36:49 +0800

    change matplotlib version

--- rasa/misc/script.sh
+++ rasa/misc/script.sh
@@ -3,4 +3,4 @@
 # pip install -U -f /home/pi/wheels -f https://tf.kmtea.eu/whl/stable.html rasa[jieba,transformers]==3.1.0 matplotlib==3.3.3 tokenizers==0.10.2 ckip-transformers
 # pip install -U --no-deps numpy==1.22.3
 
-pip install -U -f /home/pi/github/asar/rasa/misc/wheels -f https://tf.kmtea.eu/whl/stable.html rasa[jieba,transformers]==3.2.8 tokenizers==0.10.2
+pip install -U -f /home/pi/github/asar/rasa/misc/wheels -f https://tf.kmtea.eu/whl/stable.html rasa[jieba,transformers]==3.2.8 matplotlib==3.3.3 tokenizers==0.10.2
